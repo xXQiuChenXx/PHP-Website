af00b2e011fd785b3fd089e67720c609af4c13dd
xXQiuChenXx <lautaihong2004@gmail.com>
2024-07-11 22:15:20 +0800

    update

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <input type="text" name="name" id="name">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
+</html>
+
 <?php
-echo "Hello, World!";
+    if(isset($_POST['name'])){
+        $name = $_POST['name'];
+        echo "Hello $name world";
+    }
 ?>
