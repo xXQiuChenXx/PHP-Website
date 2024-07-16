a6e0e816f6aa4608af700dbdf4c8fc21b649aa0d
xXQiuChenXx <lautaihong2004@gmail.com>
2024-07-16 22:11:12 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
+<?php
 return [
     'host' => 'localhost',
-    'username' => 'your_username',
-    'password' => 'your_password',
-    'database' => 'your_database'
+    'username' => 'your_database_username',
+    'password' => 'your_database_password',
+    'database' => 'your_database_name',
 ];
