528313ad967297fae9c16e9b6516cc524478cea5
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-07-31 12:02:22 +0800

    Update sign.php

--- login-page/sign.php
+++ login-page/sign.php
@@ -1,6 +1,12 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     include 'connect.php';
+
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    echo $email;
+    echo $password;
 }
 ?>
 
