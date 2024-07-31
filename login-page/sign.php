2182ae0215f947e8b00ca8f1ee7a5de4376a1d4f
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-07-31 11:59:31 +0800

    Update sign.php

--- login-page/sign.php
+++ login-page/sign.php
@@ -1,3 +1,9 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    include 'connect.php';
+}
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -9,11 +15,13 @@
 </head>
 
 <body>
+
     <div class="container my-5">
-        <form>
+        <h1 class="text-center fw-bold">Sign Up Page</h1>
+        <form action="sign.php" method="POST">
             <div class="mb-3">
                 <label for="email" class="form-label">Email address</label>
-                <input type="email"  class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter your email address">
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter your email address">
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
@@ -21,10 +29,10 @@
                 <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password">
             </div>
             <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                <input type="checkbox" class="form-check-input" id="remember">
+                <label class="form-check-label" for="remember">Remember me</label>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary w-100">Submit</button>
         </form>
     </div>
 
